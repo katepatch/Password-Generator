f6cfae47575dd3a4b6bb2d3f98314e32bd75fd20
kate patch <kate.epatch@gmail.com>
2022-05-07 21:37:13 -0600

    adding letter variables

--- script.js
+++ script.js
@@ -1,8 +1,90 @@
 // Assignment code here
+var alphabetLower = 
+["a",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z"]
+var alphabetUpper = 
+["A",
+"B",
+"C",
+"D",
+"E",
+"F",
+"G",
+"H",
+"I",
+"J",
+"K",
+"L",
+"M",
+"N",
+"O",
+"P",
+"Q",
+"R",
+"S",
+"T",
+"U",
+"V",
+"W",
+"X",
+"Y",
+"Z"]
+var numbers = 
+[1,
+2,
+3,
+4,
+5,
+6,
+7,
+8,
+9,
+0]
+var special =
+["!",
+"@",
+"#",
+"$",
+"%",
+"^",
+"&",
+"*",
+"/",
+"_"] 
 
+function generateBtn() {
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var min = 8;
+var max = 128;
 
 // Write password to the #password input
 function writePassword() {
