52175f9bc690c08ee54b612e3cd31d5fe5ce8447
kate patch <kate.epatch@gmail.com>
2022-05-11 23:20:12 -0600

    I just keeping making my code go crazy. trying to figure out where i went wrong

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
 // Assignment code here
-var count = ""
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z"];
-var capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]; 
+var countConfirm
+var lowerConfirm
+var capitalConfirm
+var numbersConfirm
+var specialConfirm
 
 
 function generatePassword(){
-  var count = window.prompt("Let's get you a fancy new password!  Please choose between 8 and 128 characters.");
-  if (count > 128) {
+  var countConfirm = window.prompt("Let's get you a fancy new password!  Please choose between 8 and 128 characters.");
+  if (countConfirm > 128) {
     var count = prompt("Please try again.  Cannot exceed 128 characters.")
     generatePassword();
-  } else if (count < 8) {
-    var count = prompt("Please try again.  Must have more than 8 characters")
+  } else if (countConfirm < 8) {
+    var countConfirm = prompt("Please try again.  Must have more than 8 characters")
     generatePassword();
   }
-  console.log(count)
+  console.log(countConfirm)
   
   
-  var lower = window.prompt("Lowercase Letters? YES or NO")
-  lower = lower.toUpperCase();
+  var lowerConfirm = window.prompt("Lowercase Letters? YES or NO")
+  lowerConfirm = lowerConfirm.toUpperCase();
 
-  if (lower == "YES") {
+  if (lowerConfirm == "YES") {
 
   } else {
     
   };
-  console.log(lower);
-  var capital = window.prompt("Uppercase Letters? YES or NO")
-  capital = capital.toUpperCase();
-  if (capital == "YES") {
+  console.log(lowerConfirm);
+  var capitalConfirm = window.prompt("Uppercase Letters? YES or NO")
+  capitalConfirm = capitalConfirm.toUpperCase();
+  if (capitalConfirm == "YES") {
 
   } else {
     
   };
-  console.log(capital);
-  var numbers = window.prompt("Numbers? YES or NO")
-  numbers = numbers.toUpperCase();
-  if (numbers == "YES") {
+  console.log(capitalConfirm);
+  var numbersConfirm = window.prompt("Numbers? YES or NO")
+  numbersConfirm = numbersConfirm.toUpperCase();
+  if (numbersConfirm == "YES") {
 
   } else {
     
   }
-  console.log(numbers);
-  var special = window.prompt("Special Characters? YES or NO")
-  special = special.toUpperCase();
-    if (special == "YES"){
+  console.log(numbersConfirm);
+  var specialConfirm = window.prompt("Special Characters? YES or NO")
+  specialConfirm = specialConfirm.toUpperCase();
+    if (specialConfirm == "YES"){
   } else {
   };
-  console.log(special)
+  console.log(specialC)
 
 
 };
 
+function generatePassword(){
 var count = ""
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -79,14 +80,9 @@ console.log(count)
 //gonna try to make a loop to generate this code
 //var min = 8;
 //var max = 128;
-//var length = characterCount;
-//var passwordText = "";
-//for ( var i = 0; n = characterCount.length; i , length); ++i; {
-  //  String.fromCharCode(Math.floor(Math.random() * n));
-    //}
-    
+  
 
-
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
