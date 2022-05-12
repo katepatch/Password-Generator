320e37acc660d09330a5f513ba8426ddf55a4b26
kate patch <kate.epatch@gmail.com>
2022-05-11 21:45:15 -0600

    gave characterCount a value

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-var characterCount
+var characterCount = ""
 var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z"];
 var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
