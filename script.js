4556a4433652c0533c513b3591c7458010f06092
kate patch <kate.epatch@gmail.com>
2022-05-12 23:56:12 -0600

    add all of the else if statements I could think of. Now just have to debug it

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var lowerCapital = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerNumbers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowerSpecial = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var capitalNumbers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var capitalSpecial = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var numbersSpecial = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var allChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var capitalNumbers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var capitalSpecial = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var numbersSpecial = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var allChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 function generatePassword(){
   var countConfirm = window.prompt("Let's get you a fancy new password!  Please choose between 8 and 128 characters.");
@@ -75,22 +75,50 @@ function generatePassword(){
   else if (lowerConfirm == true && capitalConfirm == true && numbersConfirm == false & specialConfirm == false){
     var output = ""
     for (var i = 0; i < countConfirm; i++) {
-      output += lowerConfirm[Math.floor(Math.random() * lowerCapital.length)]
+      output += lowerCapital[Math.floor(Math.random() * lowerCapital.length)]
       console.log(output)
     }
     generatePassword = output;
-  }   else if (lowerConfirm == truee && capitalConfirm == false && numbersConfirm == true & specialConfirm == false){
+  }   else if (lowerConfirm == true && capitalConfirm == false && numbersConfirm == true & specialConfirm == false){
     var output = ""
     for (var i = 0; i < countConfirm; i++) {
-      output += lower[Math.floor(Math.random() * lower.length)]
+      output += lowerNumbers[Math.floor(Math.random() * lowerNumbers.length)]
       console.log(output)
     }
     generatePassword = output;
-  } 
+  } else if (lowerConfirm == true && capitalConfirm == false && numbersConfirm == true & specialConfirm == true){
+    var output = ""
+    for (var i = 0; i < countConfirm; i++) {
+      output += lowerSpecial[Math.floor(Math.random() * lowerSpecial.length)]
+      console.log(output)
+    }
+    generatePassword = output;
+  } else if (lowerConfirm == false && capitalConfirm == true && numbersConfirm == true & specialConfirm == false){
+    var output = ""
+    for (var i = 0; i < countConfirm; i++) {
+      output += capitalNumbers[Math.floor(Math.random() * capitalNumbers.length)]
+      console.log(output)
+    }
+    generatePassword = output;
+  } else if (lowerConfirm == false && capitalConfirm == true && numbersConfirm == false & specialConfirm == true){
+    var output = ""
+    for (var i = 0; i < countConfirm; i++) {
+      output += capitalSpecial[Math.floor(Math.random() * capitalSpecial.length)]
+      console.log(output)
+    }
+    generatePassword = output;
+  } else if (lowerConfirm == true && capitalConfirm == false && numbersConfirm == true & specialConfirm == true){
+    var output = ""
+    for (var i = 0; i < countConfirm; i++) {
+      output += numbersSpecial[Math.floor(Math.random() * numbersSpecial.length)]
+      console.log(output)
+    }
+    generatePassword = output;
+  }
+  generatePassword = output;
   
   
-  
-return generatePassword;
+return generatePassword();
   
 }
 
