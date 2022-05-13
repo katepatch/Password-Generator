6c25f2597eeb5334395871885e8ecc451b32fab2
kate patch <kate.epatch@gmail.com>
2022-05-12 21:47:28 -0600

    changed up the vars

--- script.js
+++ script.js
@@ -30,8 +30,6 @@ function generatePassword(){
   var specialConfirm = confirm("Confirm Special Characters?")
   
   console.log(specialConfirm)
-
-
 };
 
 
@@ -53,10 +51,10 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var count = ""
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "1234567890"
-var special = "!@#$%^&*()"; 
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z"];
+  var capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]; 
 
 if (lowerConfirm){
   count = count + lower;
